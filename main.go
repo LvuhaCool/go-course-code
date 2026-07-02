eb80e640ac18014d2dee48d94ebd8fe274cbb5f1
LvuhaCool <ghabaruk@gmail.com>
2026-07-02 18:22:33 +0300

    some more logical operators

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	score := 17
+	score := 177
 
-	if score == 12 {
-		fmt.Println("Дюжина")
-	} else if score == 21 {
-		fmt.Println("Очко")
-	} else if score == 50 {
-		fmt.Println("Полтинник")
+	if score < 6 || score > 16 {
+		fmt.Println("Ты попал в петушиную зону!")
+	} else if score > 8 && score < 15 {
+		fmt.Println("Ты попал в яблочко!")
+	}
+
+	if score >= 10 {
+		fmt.Println("Ты красавчик!")
 	} else {
-		fmt.Println("Ты не попал ни в какую категорию")
+		fmt.Println("Тебе нужно еще многому научиться!")
+	}
+
+	if score == 7 {
+		fmt.Println("Ты проиграл!")
+	} else if score != 7 {
+		fmt.Println("Ты выиграл!")
 	}
 }
