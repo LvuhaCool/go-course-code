971a99af23fe00b39699499be5224629a67d8c2f
LvuhaCool <ghabaruk@gmail.com>
2026-07-04 15:14:35 +0300

    smth with ifelse (last, next - loops

--- main.go
+++ main.go
@@ -3,23 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	score := 177
-
-	if score < 6 || score > 16 {
-		fmt.Println("Ты попал в петушиную зону!")
-	} else if score > 8 && score < 15 {
-		fmt.Println("Ты попал в яблочко!")
+	subscribed := true
+	if !subscribed {
+		fmt.Println("Подпишись!")
 	}
-
-	if score >= 10 {
-		fmt.Println("Ты красавчик!")
-	} else {
-		fmt.Println("Тебе нужно еще многому научиться!")
-	}
-
-	if score == 7 {
-		fmt.Println("Ты проиграл!")
-	} else if score != 7 {
-		fmt.Println("Ты выиграл!")
+	notSubscribed := true
+	if !notSubscribed {
+		fmt.Println("Ура! Ты подписан!")
 	}
 }
