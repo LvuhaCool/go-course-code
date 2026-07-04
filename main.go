280a65bfe19dd08c473d88b6722719b4dbc56688
LvuhaCool <ghabaruk@gmail.com>
2026-07-04 17:15:31 +0300

    learned abt never-ending loops + break loop operator

--- main.go
+++ main.go
@@ -1,27 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	score := 0
-
-	fmt.Println("Get Ready!")
-
-	for i := 0; i < 3; i++ {
-		fmt.Println("Вы подлетаете к трубе!")
-		fmt.Println("*Подлетаете*")
-		fmt.Println("")
-
-		fmt.Println("Вы пролетаете через трубу!")
-		fmt.Println("*Пролетаете*")
-		fmt.Println("")
-
-		fmt.Println("Вы пролетели через трубу!")
-		fmt.Println("*Пролетели*")
-		fmt.Println("")
-
-		score++
-		fmt.Println("Ваш счет:", score)
-		fmt.Println("")
-	}
+	fmt.Println("Random number:", rand.Intn(10))
 }
