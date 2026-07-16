e04095fadab66f4c0ebd443b5813fce5b9b71086
LvuhaCool <ghabaruk@gmail.com>
2026-07-16 16:20:53 +0300

    learned basic interface syntax + wrote some code i don't understand, will rewatch this part on 1x instead of 2x speed

--- payments/payments.go
+++ payments/payments.go
@@ -9,13 +9,20 @@ type PaymentModule struct {
 	paymentMethod PaymentMethod
 }
 
-func NewPaymentModule(PaymentMethod PaymentMethod) PaymentModule {
-	return PaymentModule{
-		paymentMethod: paymentMethod(),
+func NewPaymentModule(paymentMethod PaymentMethod) *PaymentModule {
+	return &PaymentModule{
+		paymentMethod: paymentMethod,
 	}
 }
 
-func (p PaymentModule) Pay(description string, usd int) int {}
+func (p PaymentModule) Pay(description string, usd int) int {
+	id := p.paymentMethod.Pay(usd)
+	info := PaymentInfo{
+		Description: description,
+		Usd:         usd,
+		Cancelled:   false,
+	}
+}
 
 func (p PaymentModule) Cancel() {}
 
