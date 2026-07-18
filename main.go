51582494e810098eb6fa4fc4a98a38abbedba40c
LvuhaCool <ghabaruk@gmail.com>
2026-07-18 19:49:03 +0300

    learned error and panic handling

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 
 	"github.com/k0kubun/pp"
 )
@@ -18,22 +19,67 @@ func Pay(user *User, usd int) error {
 		return err
 	}
 	user.Balance -= usd
-	return ""
+	return nil
+}
+
+type Car struct {
+	Armor int
+}
+
+func (c *Car) Gas() (int, error) {
+	if c.Armor-10 <= 0 {
+		return 0, errors.New("Мы не стали газовать, чтобы не сломать машину")
+	}
+
+	kmph := rand.Intn(151)
+
+	c.Armor -= 10
+
+	return kmph, nil
 }
 
 func main() {
+
+	defer func() {
+		p := recover()
+
+		if p != nil {
+			fmt.Println("Была паника:", p)
+		}
+	}()
+
+	slice := []int{1, 2, 3}
+	fmt.Println(slice[4])
+
 	user := User{
 		Name:    "Олег",
 		Balance: 10,
 	}
 
 	pp.Println("User до:", user)
-	str := Pay(&user, 15)
+	err := Pay(&user, 15)
 	pp.Println("User после:", user)
 
-	if str == "" {
-		fmt.Println("Была произведена оплата!")
+	if err != nil {
+		fmt.Println("Оплаты не было! Причина:", err.Error())
 	} else {
-		fmt.Println("Оплаты не было! Причина:", str)
+		fmt.Println("Была произведена оплата!")
+	}
+
+	car := Car{
+		Armor: 25,
+	}
+
+	for {
+		pp.Println("car до:", car)
+		kmph, err := car.Gas()
+		pp.Println("car после:", car)
+
+		if err != nil {
+			fmt.Println("Ошибка нажатия на газ:", err.Error())
+			break
+		}
+		fmt.Println("Получившийся разгон:", kmph)
+		fmt.Println("")
 	}
 }
