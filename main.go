7fd78643c823e99b264366501160f7b8f8f711f0
LvuhaCool <ghabaruk@gmail.com>
2026-08-02 12:26:59 +0300

    goroutines called with anonymous functions

--- main.go
+++ main.go
@@ -5,34 +5,13 @@ import (
 	"time"
 )
 
-func mine(transferPoint chan int, n int) {
-	fmt.Println("Поход в шахту номер", n, "начался...")
-	time.Sleep(1 * time.Second)
-	fmt.Println("Поход в шахту номер", n, "закончился")
-
-	transferPoint <- 10
-	fmt.Println("Поход номер", n, "уголь передал!")
-}
-
 func main() {
-	coal := 0
+	go func() {
+		for {
+			fmt.Println("Hello, World!")
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
 
-	transferPoint := make(chan int)
-
-	initTime := time.Now()
-
-	go mine(transferPoint, 1)
-	go mine(transferPoint, 2)
-	go mine(transferPoint, 3)
-
-	coal += <-transferPoint
 	time.Sleep(1 * time.Second)
-	coal += <-transferPoint
-	time.Sleep(1 * time.Second)
-	coal += <-transferPoint
-	time.Sleep(1 * time.Second)
-	coal += <-transferPoint
-
-	fmt.Println("Добыли", coal, "угля!")
-	fmt.Println("Прошло времени:", time.Since(initTime))
 }
